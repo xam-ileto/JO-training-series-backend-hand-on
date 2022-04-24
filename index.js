0a1d449be3bba9ac20774a3e851547fc71436232
xam-ileto <xam.ileto@gmail.com>
2022-04-25 03:51:09 +0800

    created express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT);
+console.log(`port running at port ${PORT}`);
+
+// render view engine
+app.set('view engine', 'ejs');
+
+// GET routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
